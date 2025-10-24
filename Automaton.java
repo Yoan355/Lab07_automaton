6f29f6038c7767a368685335c571a70c47c83a46
Yoan <joantodri@gmail.com>
2025-10-24 11:48:30 -0400

    question 30

--- Automaton.java
+++ Automaton.java
@@ -44,31 +44,21 @@ public class Automaton
     /**
      * Update the automaton to its next state.
      */
-    public void update()
-    {
-        // Build the new state in a separate array.
-        int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
-            nextState[i] = (left + center + right) % 2;
-        }
-        state = nextState;
+    public void update() //30
+{
+    // Build the new state in a separate array.
+    int[] nextState = new int[state.length];
+    // Naively update the state of each cell
+    // based on the state of its two neighbors.
+    for(int i = 0; i < state.length; i++) {
+        int left = (i == 0) ? 0 : state[i - 1];
+        int center = state[i];
+        int right = (i + 1 < state.length) ? state[i + 1] : 0;
+        nextState[i] = (left + center + right) % 2;
     }
+    state = nextState;
+}
+
     
     /**
      * Reset the automaton.
